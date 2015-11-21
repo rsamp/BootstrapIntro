e100175f48c945e5627dcbd9c9b9d18f56c36438
Ryan Samp <sampryan@gmail.com>
2015-11-21 13:24:19 -0800

    Finish Phase 4: Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ AuthDemo::Application.routes.draw do
   resource :user, only: [:create, :new, :show] do
     resource :counter, only: [:update]
   end
+  get '/home' => 'static_pages#home'
+  get '/contact' => 'static_pages#contact'
+  get '/about' => 'static_pages#about'
 end
